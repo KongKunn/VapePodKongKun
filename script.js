7e80dc369bc365956a19159e9a3814285ffd3119
kongkunn <congdeptraiii102@gmail.com>
2022-11-18 04:07:08 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let items = document.getElementsByClassName("aa");
+console.log(items.target);
+// items.forEach((aa) => {
+//   aa.addEventListener("click", function (e) {});
+// });
+
+// const bar = document.getElementById;
+
+// const navbar =document.getElementById('nav');
